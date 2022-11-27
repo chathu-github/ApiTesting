5e12a532dfd132e226c7503a218dbc8502b07777
chathu-github <maheshi_wickramasingha@aol.com>
2022-11-27 15:41:56 +0000

    Initial commit

---
+++ src/main/java/com/fin/ApiTesting/ApiController.java
@@ -0,0 +1,22 @@
+package com.fin.ApiTesting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+
+public class ApiController {
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "Hello";
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/fin/ApiTesting/model/Employee.java
@@ -0,0 +1,64 @@
+package com.fin.ApiTesting.model;
+
+
+public class Employee {
+
+    private long id;
+
+    public Employee(long id, String name, String department, float salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    private String department;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    private float salary;
+
+
+}
